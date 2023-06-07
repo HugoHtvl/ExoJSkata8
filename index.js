9c48c99b2c89ad30c1b52397e862bcb203419f65
HugoHtvl <hug.oo@hotmail.fr>
2023-06-07 19:11:33 +0200

    creation of a new file

---
+++ index.js
@@ -0,0 +1,109 @@
+// EXERCICE 1
+
+// Crée une fonction qui prend deux paramètres (a et b) et retourne la somme multipliée des deux paramètres.
+// Exemple : a = 2, b = 3 => 2 * 3 = 6 => 6 * 2 = 12
+
+// EXERCICE 2
+
+// Crée une fonction qui prend en paramètre un tableau et qui retourne le premier élément du tableau.
+// Exemple : [1, 2, 3] => 1
+
+// EXERCICE 3
+
+// Crée une fonction qui prend en paramètre un tableau et qui doit supprimer le dernier élément du tableau.
+// Exemple : [1, 2, 3] => [1, 2]
+
+// EXERCICE 4
+
+// Crée une fonction qui prend en paramètre un tableau comprennant des nombres et qui doit retourner la somme de tous les nombres du tableau.
+// Exemple : [1, 2, 3] => 6
+
+// EXERCICE 5
+
+// Crée une fonction qui prend en paramètre une string et qui doit retourner la string inversée.
+// Exemple : "Hello" => "olleH"
+// Pour cette exercice on va utiliser la méthode split() qui permet de transformer une string en tableau. Et la méthode reverse() qui permet d'inverser un tableau :
+
+// EXERCICE 6
+
+// Crée une fonction qui prend en paramètre un tableau et qui doit retourner le plus grand nombre du tableau.
+// Exemple : [1, 2, 3] => 3
+// Pour cette exercice on va utiliser la méthode Math.max() qui permet de retourner le plus grand nombre d'un tableau :
+
+// EXERCICE 7
+
+// Crée une fonction qui prend en paramètre un tableau et qui doit retourner le plus petit nombre du tableau.
+// Exemple : [1, 2, 3] => 1
+// Pour cette exercice on va utiliser la méthode Math.min() qui permet de retourner le plus petit nombre d'un tableau :
+
+// EXERCICE 8
+
+// Crée une fonction qui prend en paramètre une string et qui doit retourner la string sans les voyelles.
+// Exemple : "Hello" => "Hll"
+// Pour cette exercice on va utiliser la méthode split() qui permet de transformer une string en tableau. Et la méthode filter() qui permet de filtrer un tableau :
+
+// EXERCICE 9
+
+// Crée une fonction qui prend en paramètre un tableau et qui doit retourner le tableau trié par ordre croissant.
+// Exemple : [1, 3, 2] => [1, 2, 3]
+// Pour cette exercice on va utiliser la méthode sort() qui permet de trier un tableau :
+
+// EXERCICE 10
+
+// Crée une fonction qui prend en paramètre une string et qui renvoie un tableau avec toutes les rotations possibles de cette string.
+// Exemple : "Hello" => ["elloH", "lloHe", "loHel", "oHell", "Hello"]
+// Pour cette exercice on va utiliser une boucle for et la méthode push() qui permet d'ajouter un élément à un tableau :
+
+// EXERCICE 11
+
+// Crée une fonction qui prend en paramètre un tableau et qui doit ajouter le nombre 1 à chaque élément du tableau.
+// Exemple : [1, 2, 3] => [2, 3, 4]
+
+// EXERCICE 12
+
+// Crée une fonction qui prend en paramètre un tableau de string et qui doit retourner un tableau avec la longueur de chaque string.
+// Exemple : ["Hello", "World"] => [5, 5]
+
+// EXERCICE 13
+
+// Crée une fonction qui prend en paramètre un tableau de string et qui doit retourner un tableau avec toutes les premières lettres de chaque string.
+// Exemple : ["Hello", "World"] => ["H", "W"]
+
+// EXERCICE 14
+
+// Crée une fonction qui prend en paramètre un tableau de string et qui doit retourner un tableau avec toutes les dernières lettres de chaque string.
+// Exemple : ["Hello", "World"] => ["o", "d"]
+// Indice regarde l'exercice 13, c'est exactement la même chose sauf qu'il faut récupérer la dernière lettre et non la première.
+
+// EXERCICE 15
+
+// Crée une fonction qui prend en paramètre un tableau de string et qui doit renvoyer tout les string qui ont une longueur de 5 caractères ou plus.
+// Exemple : ["Hello", "World","Test", "Salut", "Yo"] => ["Hello", "World", "Salut"]
+// // Pour cette exercice on va utiliser la méthode filter() qui permet de filtrer un tableau, hésitez pas à regarder l'exercice 8 pour voir comment ça fonctionne. Sauf qu'ici on va filtrer en fonction de la longueur de la string. Incice : la méthode length() permet de récupérer la longueur d'une string.
+
+// EXERCICE 16
+
+// Crée une fonction qui prend en paramètre un tableau de nombre et qui doit retourner la somme de tout les nombres du tableau.
+// Exemple : [1, 2, 3] => 6
+
+// EXERCICE 17
+
+// Crée une fonction qui prend en deux paramètres qui sont deux tableaux de string et qui doit retourner un tableau avec tout les éléments des deux tableaux.
+// Exemple : ["Hello", "World"] et ["Test", "Salut"] => ["Hello", "World", "Test", "Salut"]
+
+// EXERCICE 18
+
+// Crée une fonction qui prend en paramètre un tableau de string et qui doit retourner toutes les string qui ont la lettre "e".
+// Exemple : ["Poulet", "Chat", "Chien", "Cheval"] => ["Poulet", "Chien", "Cheval"]
+// Pour cette exercice on va utiliser la méthode filter() qui permet de filtrer un tableau, hésitez pas à regarder l'exercice 8 pour voir comment ça fonctionne. Sauf qu'ici on va filtrer en fonction de la présence de la lettre "e". Indice : la méthode includes() permet de savoir si une string contient une lettre ou un mot.
+
+// EXERCICE 19
+
+// Crée une fonction qui prend en paramètre un tableau de nombre et qui doit retourner tout les nombres qui sont pairs par ordre croissant.
+// Exemple : [ 2, 9, 6, 5, 6] => [2, 6, 6]
+// Indice : il y a plusieurs façon de faire, soit avec un for, soit avec la méthode filter() une fois ça fait utilise la méthode sort() qui permet de trier un tableau.
+
+// EXERCICE 20
+
+// Créer une fonction qui prend un nombre qui représente le nombre de tours d'un jeu de roulette et renvoie un tableau avec les numéros de chaque tour.
+// exemple : roulette(5) ➞ [1, 2, 3, 4, 5]
